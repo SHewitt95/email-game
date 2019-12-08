9e9315f562561821d8bf92dcd601c1337b1297d9
SHewitt95 <hewitt.sherman@gmail.com>
2019-12-08 11:02:19 -0500

    getting off the ground. core mechanic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
+// https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+export const useInterval = (callback, delay) => {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+};
+
 function App() {
+  const [counter, setCounter] = useState(10);
+
+  useInterval(() => {
+    const random = Math.random();
+    const FIRST_THRESHOLD = 0.00 < random && random < 0.20;
+    const SECOND_THRESHOLD = 0.20 < random && random < 0.70;
+    const THIRD_THRESHOLD = 0.70 < random && random < 0.95;
+    const FOURTH_THRESHOLD = 0.95 < random && random < 1.0;
+    if (FIRST_THRESHOLD) {
+      // do nothing
+    } else if (SECOND_THRESHOLD) {
+      setCounter(counter + 1);
+    } else if (THIRD_THRESHOLD) {
+      setCounter(counter + 10);
+    } else if (FOURTH_THRESHOLD) {
+      setCounter(counter + 100);
+    }
+  }, 1000);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => setCounter(() => {
+        if (counter === 0) return 0;
+        return counter - 1;
+      })}>Read Email</button>
+      <p>Unread Emails: {counter}</p>
+    </>
   );
 }
 
